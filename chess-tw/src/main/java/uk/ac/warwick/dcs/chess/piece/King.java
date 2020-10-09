778f3037b5c44b6c6114bc20f0df604d5047e441
Jesutofunmi Buraimoh <43114162+jesutbura00@users.noreply.github.com>
2020-10-09 14:40:45 +0100

    Update King.java

--- chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/King.java
+++ chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/King.java
@@ -9,113 +9,128 @@ public class King extends ChessPiece {
     @Override
     public Move[] getAvailableMoves() {
         availableMoves.clear();
+        int forwards = -1;
+        if(!this.isWhite){
+            forwards = 1;
+        }
+
+        int startRank = 1;
+        if(this.isWhite)
+            startRank = 6;
 
         int moveH, moveV;
-
-        // add forward moves 
-        moveV = this.vertical + 1;
+        moveV = this.vertical + forwards;
         moveH = this.horizontal;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
         }
 
-        // add backwards moves 
-        moveV = this.vertical - 1;
-        moveH = this.horizontal;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
         }
 
-        // add right moves 
+        moveV = this.vertical - forwards;
+        moveH = this.horizontal;
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
+        }
+
         moveV = this.vertical;
         moveH = this.horizontal + 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
         }
 
-        // add left moves 
         moveV = this.vertical;
         moveH = this.horizontal - 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
         }
 
-        // add diag F-R moves
         moveV = this.vertical + 1;
         moveH = this.horizontal + 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
         }
 
-        // add diag F-L moves
-        moveV = this.vertical + 1;
-        moveH = this.horizontal - 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
-        }
-
-        // add diag B-R moves
-        moveV = this.vertical - 1;
-        moveH = this.horizontal + 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
-        }
-
-        // add diag B-L moves
         moveV = this.vertical - 1;
         moveH = this.horizontal - 1;
-        if (board.locationValid(moveV,moveH) && board.testCheck(!this.isWhite) == false && (board.pieceAtLocation(moveV,moveH) == null || board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite)) {
-            if (board.pieceAtLocation(moveV,moveH) == null) {
-                Move m = new Move(this, moveV, moveH, false);
-                availableMoves.add(m);
-            } else {
-                Move m = new Move(this, moveV, moveH, true);
-                availableMoves.add(m);
-            }
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
         }
         
+        moveV = this.vertical + 1;
+        moveH = this.horizontal - 1;
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical - 1;
+        moveH = this.horizontal + 1;
+        
+        //We can move forwards if there is an empty square in front
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && !(board.testCheckMate(this.isWhite))) { //Empty square ahead
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) != null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite && !(board.testCheckMate(this.isWhite))) {
+            //Take a piece ahead
+            Move m = new Move(this, moveV,moveH, true);
+            availableMoves.add(m);
+        }
         return availableMoves.movesToArray();
     }
 }

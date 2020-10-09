06206061be19a2336897a76eafa6a9c8b2d2a9f9
StavrosSavva <65660991+StavrosSavva@users.noreply.github.com>
2020-10-09 13:42:44 +0100

    Update Knight.java

--- chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/Knight.java
+++ chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/Knight.java
@@ -9,6 +9,51 @@ public class Knight extends ChessPiece {
     @Override
     public Move[] getAvailableMoves() {
         availableMoves.clear();
+        
+
+        int startRankx = 0;
+        int startRanky =1;
+        if(this.isWhite)
+            startRankx = 7;
+
+        int moveH, moveV;
+        moveV = this.vertical + 2;
+        moveH = this.horizontal+1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == nul && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhitel) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical - 2;
+        moveH = this.horizontal+1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) {
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical + 2;
+        moveH = this.horizontal-1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical - 2;
+        moveH = this.horizontal-1;
+        
+       
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+
 
         return availableMoves.movesToArray();
     }

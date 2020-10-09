7340f8a272dfb3e2b32cd163eb83193ccb7aa2a7
StavrosSavva <65660991+StavrosSavva@users.noreply.github.com>
2020-10-09 13:29:13 +0100

    Update Rook.java

--- chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/Rook.java
+++ chess-tw/src/main/java/uk/ac/warwick/dcs/chess/piece/Rook.java
@@ -1,14 +1,59 @@
 package uk.ac.warwick.dcs.chess.piece;
 import uk.ac.warwick.dcs.chess.*;
-public class Rook extends ChessPiece {
+public class Knight extends ChessPiece {
     private static final long serialVersionUID = 1L;
-    public Rook(Board board, boolean isWhite, int vertical, int horizontal) {
-        super(board, isWhite, (char)((int)ChessIcons.W_ROOK + (isWhite ? 0 : 6)), vertical, horizontal);
+    public Knight(Board board, boolean isWhite, int vertical, int horizontal) {
+        super(board, isWhite, (char)((int)ChessIcons.W_KNIGHT + (isWhite ? 0 : 6)), vertical, horizontal);
     }
 
     @Override
     public Move[] getAvailableMoves() {
         availableMoves.clear();
+        
+
+        int startRankx = 0;
+        int startRanky =1;
+        if(this.isWhite)
+            startRankx = 7;
+
+        int moveH, moveV;
+        moveV = this.vertical + 2;
+        moveH = this.horizontal+1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == nul && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhitel) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical - 2;
+        moveH = this.horizontal+1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) {
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical + 2;
+        moveH = this.horizontal-1;
+        
+        
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+        moveV = this.vertical - 2;
+        moveH = this.horizontal-1;
+        
+       
+        if (board.locationValid(moveV,moveH) && board.pieceAtLocation(moveV,moveH) == null && board.pieceAtLocation(moveV,moveH).isWhite != this.isWhite) { 
+            Move m = new Move(this,moveV,moveH, false);
+            availableMoves.add(m);
+        }
+
+
 
         return availableMoves.movesToArray();
     }
